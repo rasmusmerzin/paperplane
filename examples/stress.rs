48ab21c8c8d6a3cac82416d0bb0fb4809e915f24
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2021-02-26 18:41:29 +0200

    added example to test behavior with large messages

---
+++ examples/stress.rs
@@ -0,0 +1,28 @@
+use async_std::task;
+use paperplane::{Event, Server};
+
+fn main() {
+    let server = Server::<()>::new(10);
+
+    task::block_on(async {
+        server.listen("0.0.0.0:8000").await.unwrap();
+
+        while let Some(event) = server.next().await {
+            task::spawn(async move {
+                match event {
+                    Event::Message(id, msg) => {
+                        println!(
+                            "{}: {}",
+                            id,
+                            match msg.len() > 32 {
+                                true => format!("[#{}]", msg.len()),
+                                false => msg.into_text().unwrap(),
+                            }
+                        );
+                    }
+                    _ => eprintln!("{:?}", event),
+                }
+            });
+        }
+    });
+}
